f3a9e5271799fae2e0537ff5fc2366762e9d173e
rytime13579 <61478639+rytime13579@users.noreply.github.com>
2025-02-11 18:05:19 -0500

    Completed Part 3
    There should now be a header at the top of the index.html entitled "We are group 6" with a background color set to green and a corresponding box shadow.

---
+++ components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css'
+
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+    <header>
+      <h1>We are group 6!</h1>
+    </header>
+    );
+  }
+}
+export default Header;
--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+import Header from './components/header/header';
 import Example from './components/example/Example';
+
+ReactDOM.render(
+  <Header/>,
+  document.getElementById('header'),
+);
 
 ReactDOM.render(
   <Example />,
   document.getElementById('reactapp'),
 );
+
+
+
