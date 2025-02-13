95a5175368f79aa9620a445213ae9bda8d637755
rytime13579 <61478639+rytime13579@users.noreply.github.com>
2025-02-13 16:24:22 -0500

    Fixed part 3
    no longer hard coded

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './header.css';
+import '../example/Example.jsx'
 
 class Header extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      name: window.models.exampleModel().name,
+      motto: window.models.exampleModel().motto,
+      counter: 0,
+      inputValue: '',
+      buttonWasClicked: '',
+    };
   }
+  
   render() {
     return (
     <header>
-      <h1>We are group 6!</h1>
+      <h1 class="titl">
+        <code>{this.state.name}</code>
+      </h1>
+      <p class="des">
+        <code>{this.state.motto}</code>
+      </p>
     </header>
     );
   }
